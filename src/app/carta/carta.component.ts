954b02b539e5a7fb64ead531bfdd5a078b80caa2
AbnerZ0 <019201832d@uandina.edu.pe>
2022-10-05 18:11:25 -0500

    primer

--- src/app/carta/carta.component.ts
+++ src/app/carta/carta.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carta.component.css']
 })
 export class CartaComponent implements OnInit {
-  public img:string="https://www.sccnewsbyte.co.uk/wp-content/uploads/2020/08/securitythumbnail400x400.png";
+  public img:string="https://as01.epimg.net/meristation/imagenes/2022/05/15/noticias/1652600213_062393_1652616420_portada_normal.jpg";
   public Titulo:string="Galeria Fotos";
   constructor() { }
 
